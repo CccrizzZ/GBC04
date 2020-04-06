da00ff0a0610d3dfa911cd91451e43a6a336a944
Christopher Liu <30523986+CccrizzZ@users.noreply.github.com>
2020-04-05 22:07:39 -0400

    ##

--- 3111/A3/Week3/A3.cpp
+++ 3111/A3/Week3/A3.cpp
@@ -1,5 +1,5 @@
 ﻿//***************************************************************************************
-// Week6-1-A2-Transparency.cpp 
+// Week6-1-A3-Transparency.cpp 
 //0 alpha means 0 % opaque, 0.4 means 40 % opaque, and 1.0 means 100 % opaque
 //The relationship between opacity and transparency:
 //T = 1− A, where A is opacity and T is transparency
@@ -66,13 +66,13 @@ enum class RenderLayer : int
 	Count
 };
 
-class A2 : public D3DApp
+class A3 : public D3DApp
 {
 public:
-	A2(HINSTANCE hInstance);
-	A2(const A2& rhs) = delete;
-	A2& operator=(const A2& rhs) = delete;
-	~A2();
+	A3(HINSTANCE hInstance);
+	A3(const A3& rhs) = delete;
+	A3& operator=(const A3& rhs) = delete;
+	~A3();
 
 	virtual bool Initialize()override;
 
@@ -167,7 +167,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 
 	try
 	{
-		A2 theApp(hInstance);
+		A3 theApp(hInstance);
 		if (!theApp.Initialize())
 			return 0;
 
@@ -180,18 +180,18 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 	}
 }
 
-A2::A2(HINSTANCE hInstance)
+A3::A3(HINSTANCE hInstance)
 	: D3DApp(hInstance)
 {
 }
 
-A2::~A2()
+A3::~A3()
 {
 	if (md3dDevice != nullptr)
 		FlushCommandQueue();
 }
 
-bool A2::Initialize()
+bool A3::Initialize()
 {
 	if (!D3DApp::Initialize())
 		return false;
@@ -231,7 +231,7 @@ bool A2::Initialize()
 	return true;
 }
 
-void A2::OnResize()
+void A3::OnResize()
 {
 	D3DApp::OnResize();
 
@@ -242,7 +242,7 @@ void A2::OnResize()
 	XMStoreFloat4x4(&mProj, P);
 }
 
-void A2::Update(const GameTimer& gt)
+void A3::Update(const GameTimer& gt)
 {
 	OnKeyboardInput(gt);
 	UpdateCamera(gt);
@@ -268,7 +268,7 @@ void A2::Update(const GameTimer& gt)
 	UpdateWaves(gt);
 }
 
-void A2::Draw(const GameTimer& gt)
+void A3::Draw(const GameTimer& gt)
 {
 	auto cmdListAlloc = mCurrFrameResource->CmdListAlloc;
 
@@ -339,7 +339,7 @@ void A2::Draw(const GameTimer& gt)
 	mCommandQueue->Signal(mFence.Get(), mCurrentFence);
 }
 
-void A2::OnMouseDown(WPARAM btnState, int x, int y)
+void A3::OnMouseDown(WPARAM btnState, int x, int y)
 {
 	mLastMousePos.x = x;
 	mLastMousePos.y = y;
@@ -347,12 +347,12 @@ void A2::OnMouseDown(WPARAM btnState, int x, int y)
 	SetCapture(mhMainWnd);
 }
 
-void A2::OnMouseUp(WPARAM btnState, int x, int y)
+void A3::OnMouseUp(WPARAM btnState, int x, int y)
 {
 	ReleaseCapture();
 }
 
-void A2::OnMouseMove(WPARAM btnState, int x, int y)
+void A3::OnMouseMove(WPARAM btnState, int x, int y)
 {
 
     if((btnState & MK_LBUTTON) != 0)
@@ -365,7 +365,7 @@ void A2::OnMouseMove(WPARAM btnState, int x, int y)
 	mLastMousePos.y = y;
 }
 
-void A2::OnKeyboardInput(const GameTimer& gt)
+void A3::OnKeyboardInput(const GameTimer& gt)
 {
 	const float dt = gt.DeltaTime();
 	
@@ -398,12 +398,12 @@ void A2::OnKeyboardInput(const GameTimer& gt)
 
 }
 
-void A2::UpdateCamera(const GameTimer& gt)
+void A3::UpdateCamera(const GameTimer& gt)
 {
 
 }
 
-void A2::AnimateMaterials(const GameTimer& gt)
+void A3::AnimateMaterials(const GameTimer& gt)
 {
 	// Scroll the water material texture coordinates.
 	auto waterMat = mMaterials["water"].get();
@@ -427,7 +427,7 @@ void A2::AnimateMaterials(const GameTimer& gt)
 	waterMat->NumFramesDirty = gNumFrameResources;
 }
 
-void A2::UpdateObjectCBs(const GameTimer& gt)
+void A3::UpdateObjectCBs(const GameTimer& gt)
 {
 	auto currObjectCB = mCurrFrameResource->ObjectCB.get();
 	for (auto& e : mAllRitems)
@@ -451,7 +451,7 @@ void A2::UpdateObjectCBs(const GameTimer& gt)
 	}
 }
 
-void A2::UpdateMaterialCBs(const GameTimer& gt)
+void A3::UpdateMaterialCBs(const GameTimer& gt)
 {
 	auto currMaterialCB = mCurrFrameResource->MaterialCB.get();
 	for (auto& e : mMaterials)
@@ -477,7 +477,7 @@ void A2::UpdateMaterialCBs(const GameTimer& gt)
 	}
 }
 
-void A2::UpdateMainPassCB(const GameTimer& gt)
+void A3::UpdateMainPassCB(const GameTimer& gt)
 {
 	//XMMATRIX view = XMLoadFloat4x4(&mView);
 	//XMMATRIX proj = XMLoadFloat4x4(&mProj);
@@ -518,8 +518,8 @@ void A2::UpdateMainPassCB(const GameTimer& gt)
 	mMainPassCB.Lights[2].Position = { 4.0f, 40.0f, -2.0f };
 	mMainPassCB.Lights[2].Strength = { 10.0f, 1.0f, 4.0f };
 
-	mMainPassCB.Lights[3].Position = { -36.0f, 17.0f, -5.0f };
-	mMainPassCB.Lights[3].Strength = { 6.0f, 2.0f, 10.0f };
+	// mMainPassCB.Lights[3].Position = { -36.0f, 17.0f, -5.0f };
+	// mMainPassCB.Lights[3].Strength = { 6.0f, 2.0f, 10.0f };
 
 	mMainPassCB.Lights[4].Position = { -13.0f, 10.0f, -10.0f };
 	mMainPassCB.Lights[4].Strength = { 10.0f, 1.0f, 1.0f };
@@ -542,13 +542,13 @@ void A2::UpdateMainPassCB(const GameTimer& gt)
 	mMainPassCB.Lights[10].Position = { 35.0f, 4.5f, -16.5f };
 	mMainPassCB.Lights[10].Strength = { 2.0f, 2.0f, 1.0f };
 
-	mMainPassCB.Lights[11].Position = { -35.0f, 5.0f, -12.0f };
+	mMainPassCB.Lights[11].Position = { -35.0f, 4.5f, -16.5f };
 	mMainPassCB.Lights[11].Strength = { 2.0f, 2.0f, 1.0f };
 	
 	mMainPassCB.Lights[12].Position = { 35.0f, 4.5f, 16.5f };
 	mMainPassCB.Lights[12].Strength = { 2.0f, 2.0f, 1.0f };
 
-	mMainPassCB.Lights[13].Position = { -35.0f, 5.0f, 12.0f };
+	mMainPassCB.Lights[13].Position = { -35.0f, 4.5f, 16.5f };
 	mMainPassCB.Lights[13].Strength = { 2.0f, 2.0f, 1.0f };
 
 	// spot lights
@@ -556,15 +556,18 @@ void A2::UpdateMainPassCB(const GameTimer& gt)
 	mMainPassCB.Lights[14].Strength = { 3.0f, 3.0f, 1.0f };
 	mMainPassCB.Lights[14].SpotPower = 1.0f;
 
-	mMainPassCB.Lights[15].Position = { 9.0f, 5.0f, -12.0f };
+	mMainPassCB.Lights[15].Position = { 9.0f, 4.5f, -16.5f };
 	mMainPassCB.Lights[15].Strength = { 3.0f, 3.0f, 1.0f };
 	mMainPassCB.Lights[15].SpotPower = 1.0f;
 
+	mMainPassCB.Lights[3].Position = { 28.0f, 12.0f, -5.0f };
+	mMainPassCB.Lights[3].Strength = { 1.0f, 1.0f, 3.0f };
+	
 	auto currPassCB = mCurrFrameResource->PassCB.get();
 	currPassCB->CopyData(0, mMainPassCB);
 }
 
-void A2::UpdateWaves(const GameTimer& gt)
+void A3::UpdateWaves(const GameTimer& gt)
 {
 	// Every quarter second, generate a random wave.
 	static float t_base = 0.0f;
@@ -604,7 +607,7 @@ void A2::UpdateWaves(const GameTimer& gt)
 	mWavesRitem->Geo->VertexBufferGPU = currWavesVB->Resource();
 }
 
-void A2::LoadTextures()
+void A3::LoadTextures()
 {
 	auto grassTex = std::make_unique<Texture>();
 	grassTex->Name = "grassTex";
@@ -709,6 +712,13 @@ void A2::LoadTextures()
 		roadTex2->Resource, roadTex2->UploadHeap));
 
 	
+	auto GBCtex = std::make_unique<Texture>();
+	GBCtex->Name = "GBCtex";
+	GBCtex->Filename = L"./Tex/gbc1.dds";
+	ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(md3dDevice.Get(),
+		mCommandList.Get(), GBCtex->Filename.c_str(),
+		GBCtex->Resource, GBCtex->UploadHeap));
+
 	mTextures[grassTex->Name] = std::move(grassTex);
 	mTextures[waterTex->Name] = std::move(waterTex);
 	mTextures[fenceTex->Name] = std::move(fenceTex);
@@ -723,6 +733,8 @@ void A2::LoadTextures()
 	mTextures[TDtex->Name] = std::move(TDtex);
 	mTextures[BMOtex->Name] = std::move(BMOtex);
 	mTextures[roadTex2->Name] = std::move(roadTex2);
+	mTextures[GBCtex->Name] = std::move(GBCtex);
+
 
 
 
@@ -732,7 +744,7 @@ void A2::LoadTextures()
 	
 }
 
-void A2::BuildRootSignature()
+void A3::BuildRootSignature()
 {
 	CD3DX12_DESCRIPTOR_RANGE texTable;
 	texTable.Init(D3D12_DESCRIPTOR_RANGE_TYPE_SRV, 1, 0);
@@ -772,13 +784,13 @@ void A2::BuildRootSignature()
 		IID_PPV_ARGS(mRootSignature.GetAddressOf())));
 }
 
-void A2::BuildDescriptorHeaps()
+void A3::BuildDescriptorHeaps()
 {
 	//
 	// Create the SRV heap.
 	//
 	D3D12_DESCRIPTOR_HEAP_DESC srvHeapDesc = {};
-	srvHeapDesc.NumDescriptors = 15;
+	srvHeapDesc.NumDescriptors = 16;
 	srvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
 	srvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
 	ThrowIfFailed(md3dDevice->CreateDescriptorHeap(&srvHeapDesc, IID_PPV_ARGS(&mSrvDescriptorHeap)));
@@ -802,6 +814,8 @@ void A2::BuildDescriptorHeaps()
 	auto TDtex = mTextures["TDtex"]->Resource;
 	auto BMOtex = mTextures["BMOtex"]->Resource;
 	auto roadTex2 = mTextures["roadTex2"]->Resource;
+	auto GBCtex = mTextures["GBCtex"]->Resource;
+
 
 
 
@@ -893,9 +907,15 @@ void A2::BuildDescriptorHeaps()
 	
 	srvDesc.Format = roadTex2->GetDesc().Format;
 	md3dDevice->CreateShaderResourceView(roadTex2.Get(), &srvDesc, hDescriptor);
+
+	// next descriptor
+	hDescriptor.Offset(1, mCbvSrvDescriptorSize);
+	
+	srvDesc.Format = GBCtex->GetDesc().Format;
+	md3dDevice->CreateShaderResourceView(GBCtex.Get(), &srvDesc, hDescriptor);
 }
 
-void A2::BuildShadersAndInputLayout()
+void A3::BuildShadersAndInputLayout()
 {
 	const D3D_SHADER_MACRO defines[] =
 	{
@@ -932,7 +952,7 @@ void A2::BuildShadersAndInputLayout()
 	};
 }
 
-void A2::BuildTreeSpritesGeometry()
+void A3::BuildTreeSpritesGeometry()
 {
 
 	struct TreeSpriteVertex
@@ -968,10 +988,19 @@ void A2::BuildTreeSpritesGeometry()
 		temp += 10;
 	}
 
+	temp = 0;
+	for (int i = 26; i < 31; i++)
+	{
+		vertices[i].Pos = XMFLOAT3(-45, 2, (temp - 20));
+		vertices[i].Size = XMFLOAT2(5.0f, 5.0f);
+		temp += 10;
+	}
+
+
 	std::array<std::uint16_t, 31> indices =
 	{
 		0, 1, 2, 3, 4, 5, 6, 7,
-		8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29
+		8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30
 	};
 
 	const UINT vbByteSize = (UINT)vertices.size() * sizeof(TreeSpriteVertex);
@@ -1007,7 +1036,7 @@ void A2::BuildTreeSpritesGeometry()
 	mGeometries["treeSpritesGeo"] = std::move(geo);
 }
 
-void A2::BuildWavesGeometry()
+void A3::BuildWavesGeometry()
 {
 	std::vector<std::uint16_t> indices(3 * mWaves->TriangleCount()); // 3 indices per face
 	assert(mWaves->VertexCount() < 0x0000ffff);
@@ -1063,7 +1092,7 @@ void A2::BuildWavesGeometry()
 	mGeometries["waterGeo"] = std::move(geo);
 }
 
-void A2::BuildBoxGeometry()
+void A3::BuildBoxGeometry()
 {
 	GeometryGenerator geoGen;
 	GeometryGenerator::MeshData box = geoGen.CreateBox(1.0f, 1.0f, 1.0f, 3);
@@ -1204,7 +1233,7 @@ void A2::BuildBoxGeometry()
 	mGeometries[geo->Name] = std::move(geo);
 }
 
-void A2::BuildPSOs()
+void A3::BuildPSOs()
 {
 	D3D12_GRAPHICS_PIPELINE_STATE_DESC opaquePsoDesc;
 
@@ -1319,7 +1348,7 @@ void A2::BuildPSOs()
 	ThrowIfFailed(md3dDevice->CreateGraphicsPipelineState(&treeSpritePsoDesc, IID_PPV_ARGS(&mPSOs["treeSprites"])));
 }
 
-void A2::BuildFrameResources()
+void A3::BuildFrameResources()
 {
 	for (int i = 0; i < gNumFrameResources; ++i)
 	{
@@ -1328,7 +1357,7 @@ void A2::BuildFrameResources()
 	}
 }
 
-void A2::BuildMaterials()
+void A3::BuildMaterials()
 {
 	auto grass = std::make_unique<Material>();
 	grass->Name = "grass";
@@ -1446,6 +1475,13 @@ void A2::BuildMaterials()
 	roadSprites2->FresnelR0 = XMFLOAT3(0.01f, 0.01f, 0.01f);
 	roadSprites2->Roughness = 0.5f;
 
+	auto GBCSprite = std::make_unique<Material>();
+	GBCSprite->Name = "GBCSprite";
+	GBCSprite->MatCBIndex = 14;
+	GBCSprite->DiffuseSrvHeapIndex = 14;
+	GBCSprite->DiffuseAlbedo = XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f);
+	GBCSprite->FresnelR0 = XMFLOAT3(0.01f, 0.01f, 0.01f);
+	GBCSprite->Roughness = 0.5f;
 
 	mMaterials["grass"] = std::move(grass);
 	mMaterials["water"] = std::move(water);
@@ -1461,6 +1497,8 @@ void A2::BuildMaterials()
 	mMaterials["TDsprites"] = std::move(TDsprites);
 	mMaterials["BMOsprites"] = std::move(BMOsprites);
 	mMaterials["roadSprites2"] = std::move(roadSprites2);
+	mMaterials["GBCSprite"] = std::move(GBCSprite);
+
 
 
 
@@ -1469,7 +1507,7 @@ void A2::BuildMaterials()
 	
 }
 
-void A2::BuildRenderItems()
+void A3::BuildRenderItems()
 {
 	auto wavesRitem = std::make_unique<RenderItem>();
 	wavesRitem->World = MathHelper::Identity4x4();
@@ -1833,12 +1871,101 @@ void A2::BuildRenderItems()
 	mRitemLayer[(int)RenderLayer::Opaque].push_back(poleComponent3.get());
 	mAllRitems.push_back(std::move(poleComponent3));
 
+	auto pole4 = std::make_unique<RenderItem>();
+	XMStoreFloat4x4(&pole4->World, XMMatrixScaling(0.5f, 3.0f, 0.5f)* XMMatrixTranslation(9.0f, 0.0f, -17.0f ));
+	XMStoreFloat4x4(&pole4->TexTransform, XMMatrixScaling(1.0f, 1.0f, 1.0f));
+	pole4->ObjCBIndex = 26;
+	pole4->Geo = mGeometries["shapeGeo"].get();
+	pole4->Mat = mMaterials["tile0"].get();
+	pole4->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	pole4->IndexCount = pole4->Geo->DrawArgs["cylinder"].IndexCount;
+	pole4->StartIndexLocation = pole4->Geo->DrawArgs["cylinder"].StartIndexLocation;
+	pole4->BaseVertexLocation = pole4->Geo->DrawArgs["cylinder"].BaseVertexLocation;
+	mRitemLayer[(int)RenderLayer::Opaque].push_back(pole4.get());
+	mAllRitems.push_back(std::move(pole4));
+
+	auto poleComponent4 = std::make_unique<RenderItem>();
+	XMStoreFloat4x4(&poleComponent4->World, XMMatrixScaling(0.5f, 0.1f, 1.5f)* XMMatrixTranslation(9.0f, 4.5f, -16.5f ));
+	XMStoreFloat4x4(&poleComponent4->TexTransform, XMMatrixScaling(1.0f, 1.0f, 1.0f));
+	poleComponent4->ObjCBIndex = 27;
+	poleComponent4->Geo = mGeometries["shapeGeo"].get();
+	poleComponent4->Mat = mMaterials["tile0"].get();
+	poleComponent4->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	poleComponent4->IndexCount = poleComponent4->Geo->DrawArgs["box"].IndexCount;
+	poleComponent4->StartIndexLocation = poleComponent4->Geo->DrawArgs["box"].StartIndexLocation;
+	poleComponent4->BaseVertexLocation = poleComponent4->Geo->DrawArgs["box"].BaseVertexLocation;
+	mRitemLayer[(int)RenderLayer::Opaque].push_back(poleComponent4.get());
+	mAllRitems.push_back(std::move(poleComponent4));
+
+
+	auto pole5 = std::make_unique<RenderItem>();
+	XMStoreFloat4x4(&pole5->World, XMMatrixScaling(0.5f, 3.0f, 0.5f)* XMMatrixTranslation(-35.0f, 0.0f, -17.0f));
+	XMStoreFloat4x4(&pole5->TexTransform, XMMatrixScaling(1.0f, 1.0f, 1.0f));
+	pole5->ObjCBIndex = 28;
+	pole5->Geo = mGeometries["shapeGeo"].get();
+	pole5->Mat = mMaterials["tile0"].get();
+	pole5->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	pole5->IndexCount = pole5->Geo->DrawArgs["cylinder"].IndexCount;
+	pole5->StartIndexLocation = pole5->Geo->DrawArgs["cylinder"].StartIndexLocation;
+	pole5->BaseVertexLocation = pole5->Geo->DrawArgs["cylinder"].BaseVertexLocation;
+	mRitemLayer[(int)RenderLayer::Opaque].push_back(pole5.get());
+	mAllRitems.push_back(std::move(pole5));
+
+	auto poleComponent6 = std::make_unique<RenderItem>();
+	XMStoreFloat4x4(&poleComponent6->World, XMMatrixScaling(0.5f, 0.1f, 1.5f)* XMMatrixTranslation(-35.0f, 4.5f, -16.5f));
+	XMStoreFloat4x4(&poleComponent6->TexTransform, XMMatrixScaling(1.0f, 1.0f, 1.0f));
+	poleComponent6->ObjCBIndex = 29;
+	poleComponent6->Geo = mGeometries["shapeGeo"].get();
+	poleComponent6->Mat = mMaterials["tile0"].get();
+	poleComponent6->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	poleComponent6->IndexCount = poleComponent6->Geo->DrawArgs["box"].IndexCount;
+	poleComponent6->StartIndexLocation = poleComponent6->Geo->DrawArgs["box"].StartIndexLocation;
+	poleComponent6->BaseVertexLocation = poleComponent6->Geo->DrawArgs["box"].BaseVertexLocation;
+	mRitemLayer[(int)RenderLayer::Opaque].push_back(poleComponent6.get());
+	mAllRitems.push_back(std::move(poleComponent6));
 
 
 
 
+	auto pole6 = std::make_unique<RenderItem>();
+	XMStoreFloat4x4(&pole6->World, XMMatrixScaling(0.5f, 3.0f, 0.5f)* XMMatrixTranslation(-35.0f, 0.0f, 17.0f));
+	XMStoreFloat4x4(&pole6->TexTransform, XMMatrixScaling(1.0f, 1.0f, 1.0f));
+	pole6->ObjCBIndex = 30;
+	pole6->Geo = mGeometries["shapeGeo"].get();
+	pole6->Mat = mMaterials["tile0"].get();
+	pole6->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	pole6->IndexCount = pole6->Geo->DrawArgs["cylinder"].IndexCount;
+	pole6->StartIndexLocation = pole6->Geo->DrawArgs["cylinder"].StartIndexLocation;
+	pole6->BaseVertexLocation = pole6->Geo->DrawArgs["cylinder"].BaseVertexLocation;
+	mRitemLayer[(int)RenderLayer::Opaque].push_back(pole6.get());
+	mAllRitems.push_back(std::move(pole6));
 
+	auto poleComponent5 = std::make_unique<RenderItem>();
+	XMStoreFloat4x4(&poleComponent5->World, XMMatrixScaling(0.5f, 0.1f, 1.5f)* XMMatrixTranslation(-35.0f, 4.5f, 16.5f));
+	XMStoreFloat4x4(&poleComponent5->TexTransform, XMMatrixScaling(1.0f, 1.0f, 1.0f));
+	poleComponent5->ObjCBIndex = 31;
+	poleComponent5->Geo = mGeometries["shapeGeo"].get();
+	poleComponent5->Mat = mMaterials["tile0"].get();
+	poleComponent5->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	poleComponent5->IndexCount = poleComponent5->Geo->DrawArgs["box"].IndexCount;
+	poleComponent5->StartIndexLocation = poleComponent5->Geo->DrawArgs["box"].StartIndexLocation;
+	poleComponent5->BaseVertexLocation = poleComponent5->Geo->DrawArgs["box"].BaseVertexLocation;
+	mRitemLayer[(int)RenderLayer::Opaque].push_back(poleComponent5.get());
+	mAllRitems.push_back(std::move(poleComponent5));
 
+	// GBC logo
+	auto GBCitem = std::make_unique<RenderItem>();
+	XMStoreFloat4x4(&GBCitem->World, XMMatrixScaling(4.0f, 2.0f, 1.5f)* XMMatrixTranslation(27.5f, 9.5f, -2.5f));
+	XMStoreFloat4x4(&GBCitem->TexTransform, XMMatrixScaling(1.0f, 1.0f, 1.0f));
+	GBCitem->ObjCBIndex = 32;
+	GBCitem->Geo = mGeometries["shapeGeo"].get();
+	GBCitem->Mat = mMaterials["GBCSprite"].get();
+	GBCitem->PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+	GBCitem->IndexCount = GBCitem->Geo->DrawArgs["box"].IndexCount;
+	GBCitem->StartIndexLocation = GBCitem->Geo->DrawArgs["box"].StartIndexLocation;
+	GBCitem->BaseVertexLocation = GBCitem->Geo->DrawArgs["box"].BaseVertexLocation;
+	mRitemLayer[(int)RenderLayer::AlphaTested].push_back(GBCitem.get());
+	mAllRitems.push_back(std::move(GBCitem));
 
 	mAllRitems.push_back(std::move(wavesRitem));
 	mAllRitems.push_back(std::move(boxRitem));
@@ -1846,7 +1973,7 @@ void A2::BuildRenderItems()
 
 }
 
-void A2::DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems)
+void A3::DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems)
 {
 	UINT objCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(ObjectConstants));
 	UINT matCBByteSize = d3dUtil::CalcConstantBufferByteSize(sizeof(MaterialConstants));
@@ -1880,7 +2007,7 @@ void A2::DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<R
 	}
 }
 
-std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6> A2::GetStaticSamplers()
+std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6> A3::GetStaticSamplers()
 {
 	// Applications usually only need a handful of samplers.  So just define them all up front
 	// and keep them available as part of the root signature.  
@@ -1937,12 +2064,12 @@ std::array<const CD3DX12_STATIC_SAMPLER_DESC, 6> A2::GetStaticSamplers()
 		anisotropicWrap, anisotropicClamp };
 }
 
-float A2::GetHillsHeight(float x, float z)const
+float A3::GetHillsHeight(float x, float z)const
 {
 	return 0.3f*(z*sinf(0.1f*x) + x * cosf(0.1f*z));
 }
 
-XMFLOAT3 A2::GetHillsNormal(float x, float z)const
+XMFLOAT3 A3::GetHillsNormal(float x, float z)const
 {
 	// n = (-df/dx, 1, -df/dz)
 	XMFLOAT3 n(
